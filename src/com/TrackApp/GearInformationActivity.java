e17dd1de4f86d6c682573318b7899fbe26aea6ae
Andrew Carlberg <aec1kt@tamu.edu>
2011-11-18 23:14:03 -0600

    Gear Information Page added

---
+++ src/com/TrackApp/GearInformationActivity.java
@@ -0,0 +1,12 @@
+package com.TrackApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GearInformationActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gear_information);
+	}
+}
