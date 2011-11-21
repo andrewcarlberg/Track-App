20d7a1851bbee22c1f2bbba114b31d5c2b37531c
Andrew Carlberg <aec1kt@tamu.edu>
2011-11-20 22:56:09 -0600

    Gear Page Complete

--- src/com/TrackApp/GearInformationActivity.java
+++ src/com/TrackApp/GearInformationActivity.java
@@ -1,10 +1,13 @@
 package com.TrackApp;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import com.TrackApp.NumberPicker;
 
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class GearInformationActivity extends Activity{
@@ -19,15 +22,55 @@ public class GearInformationActivity extends Activity{
 		rearCog.setRange(10,23);
 		rearCog.setCurrent(14);	
 		final TextView Gear_inches = (TextView)findViewById(R.id.Gear_Inch);
+		final SeekBar cadence_select= (SeekBar)findViewById(R.id.CadenceBar);
+		final TextView cadence = (TextView)findViewById(R.id.Cadence);
+		final TextView rollout = (TextView)findViewById(R.id.Rollout);
+		final TextView speed = (TextView)findViewById(R.id.Speed);
+		
+		//Distance Times
+		final TextView twohm = (TextView)findViewById(R.id.twohundredtime);
+		final TextView twofiftym = (TextView)findViewById(R.id.twofiftymtime);
+		final TextView threethirdythreem = (TextView)findViewById(R.id.threethirdythreemtime);
+		final TextView fourhm = (TextView)findViewById(R.id.fourhundredmtime);
+		final TextView fivehm = (TextView)findViewById(R.id.fivehundredmtime);
+		final TextView onek = (TextView)findViewById(R.id.onektime);
+		final TextView twok = (TextView)findViewById(R.id.twoktime);
+		final TextView threek = (TextView)findViewById(R.id.threektime);
+		final TextView fourk = (TextView)findViewById(R.id.fourktime);
+		
+		cadence_select.setMax(200);
+		cadence_select.setProgress(90);
 		frontRing.setOnChangeListener(new NumberPicker.OnChangedListener() {
 
 			public void onChanged(NumberPicker picker, int oldVal, int newVal) {
-				Gear_Inch_Calc from_Front = new Gear_Inch_Calc(newVal,rearCog.getCurrent());
-				
+				Gear_Inch_Calc from_Front = new Gear_Inch_Calc(newVal,rearCog.getCurrent());			
 				CharSequence x;
-				Double y= from_Front.Calc_inches();
+				Double y= from_Front.getGearInches();
 				x = (CharSequence) y.toString();
-				Gear_inches.setText(x);				
+				Gear_inches.setText(x);
+				x = (CharSequence)((Double)from_Front.getDistanceTraveled()).toString();
+				rollout.setText(x);
+				double KPH_speed = from_Front.KPH_Speed(cadence_select.getProgress());
+				x  =(CharSequence)((Double)KPH_speed).toString();
+				speed.setText(x);
+				twohm.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 200)));
+				twofiftym.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 250)));
+				threethirdythreem.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 333)));
+				fourhm.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 400)));
+				fivehm.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 500)));
+				onek.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 1000)));
+				twok.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 2000)));
+				threek.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 3000)));
+				fourk.setText((CharSequence)from_Front.Pretty_Print(
+						from_Front.Time_To_Complete(cadence_select.getProgress(), 4000)));
 			}
 		});
 		
@@ -37,15 +80,82 @@ public class GearInformationActivity extends Activity{
 				
 				Gear_Inch_Calc from_Rear = new Gear_Inch_Calc(frontRing.getCurrent(),newVal);
 				CharSequence x;
-				Double y= from_Rear.Calc_inches();
+				Double y= from_Rear.getGearInches();
 				x = (CharSequence) y.toString();
-				Gear_inches.setText(x);				
+				Gear_inches.setText(x);
+				x = (CharSequence)((Double)from_Rear.getDistanceTraveled()).toString();
+				rollout.setText(x);
+				double KPH_speed = from_Rear.KPH_Speed(cadence_select.getProgress());
+				x  =(CharSequence)((Double)KPH_speed).toString();
+				speed.setText(x);
+				
+				twohm.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 200)));
+				twofiftym.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 250)));
+				threethirdythreem.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 333)));
+				fourhm.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 400)));
+				fivehm.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 500)));
+				onek.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 1000)));
+				twok.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 2000)));
+				threek.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 3000)));
+				fourk.setText((CharSequence)from_Rear.Pretty_Print(
+						from_Rear.Time_To_Complete(cadence_select.getProgress(), 4000)));
+				
 			}
 		});
 		
+		cadence_select.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				Gear_Inch_Calc gear = new Gear_Inch_Calc(frontRing.getCurrent(),rearCog.getCurrent());
+				CharSequence x = (CharSequence)((Integer)progress).toString();
+				cadence.setText(x);
+				x = (CharSequence)((Double)gear.KPH_Speed(progress)).toString();
+				speed.setText(x);
+				
+				twohm.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 200)));
+				twofiftym.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 250)));
+				threethirdythreem.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 333)));
+				fourhm.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 400)));
+				fivehm.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 500)));
+				onek.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 1000)));
+				twok.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 2000)));
+				threek.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 3000)));
+				fourk.setText((CharSequence)gear.Pretty_Print(
+						gear.Time_To_Complete(progress, 4000)));
+				
+			}
+		});
 
 				
 		
 	}
+
 }
 
--- src/com/TrackApp/Gear_Inch_Calc.java
+++ src/com/TrackApp/Gear_Inch_Calc.java
@@ -9,21 +9,39 @@ public class Gear_Inch_Calc{
 	protected static int Effective_wheel_size = 27;
 	protected static Double Gear_Inches;
 	protected static Double Distance_Traveled;
+	private double KM_Factor = 0.62137;
 	
 	Gear_Inch_Calc(int frontRing, int cog)
 	{
 		
 		FrontRing = frontRing;
 		Cog = cog;	
+		Calc_inches();
+		Calc_distance();
 	}
 
-	protected Double Calc_inches() {
+	
+	private void Calc_inches() {
 		// TODO Auto-generated method stub
-		Gear_Inches = (double)FrontRing/(double)Cog * Effective_wheel_size;
-		return round(Gear_Inches,2);
+		Gear_Inches = round((double)FrontRing/(double)Cog * Effective_wheel_size,2);
 	}
 	
-	protected Double Calc_distance()
+	public double getGearInches()
+	{
+		return Gear_Inches;
+	}
+	
+	private void Calc_distance()
+	{
+		Distance_Traveled = round(Gear_Inches*Math.PI*0.0254,2);
+	}
+	
+	public double getDistanceTraveled()
+	{
+		return Distance_Traveled;
+	}
+	
+	private double Calc_distance_English()
 	{
 		Distance_Traveled = Gear_Inches*Math.PI;
 		return Distance_Traveled;
@@ -33,6 +51,44 @@ public class Gear_Inch_Calc{
 	    BigDecimal bd = new BigDecimal(d);
 	    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
 	    return bd.doubleValue();
+	}
+	
+	protected double Time_To_Complete(int cadence, double distance)
+	{
+		double time = distance/(KPH_Speed(cadence)*1000.0/3600.0);
+		return round(time,2);
+	}
+
+	public String Pretty_Print(double time_in_seconds)
+	{
+		//mm:ss:th
+		String Time;
+		if((int)time_in_seconds/60>0)
+		{
+			int minutes = (int)time_in_seconds/60;
+			Time = ((Integer)minutes).toString();
+			time_in_seconds = round(time_in_seconds - minutes*60,2);
+		}
+		else
+			Time="00";
+		if (time_in_seconds <10)
+			Time = Time +":0"+((Double)time_in_seconds).toString();
+		else
+			Time = Time +":"+((Double)time_in_seconds).toString();
+		
+		return Time;
+		
+		
+	}
+	
+	protected double KPH_Speed(int cadence)
+	{
+		return round(Calc_distance_English()*((double)cadence)*60.0/12.0/5280.0/KM_Factor,2);
+	}
+	
+	protected double MPH_Speed(int cadence)
+	{
+		return round(Calc_distance_English()*((double)cadence)*60.0/12.0/5280.0,2);
 	}
 }
 
